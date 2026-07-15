f7c2b1a8b14f868c719bdf580a8185161895c572
jaasminenb <izzayaserere@gmail.com>
2026-07-15 14:12:35 +0700

    minggu5

---
+++ Minggu-5/Nilai.java
@@ -0,0 +1,19 @@
+package minggu5;
+
+public class package minggu5;
+
+{
+    String nama;
+    String nim;
+    int tahunMasuk;
+    int nilaiUTS;
+    int nilaiUAS;
+
+    Nilai(String nama, String nim, int tahunMasuk, int nilaiUTS, int nilaiUAS){
+        this.nama = nama;
+        this.nim = nim;
+        this.tahunMasuk = tahunMasuk;
+        this.nilaiUTS = nilaiUTS;
+        this.nilaiUAS = nilaiUAS;
+    }
+}
